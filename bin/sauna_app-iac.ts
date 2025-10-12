6fff4be0d456e349d3268cfa9c9830fb8ac5684c
Saija Pajari <saija.m.pajari@gmail.com>
2025-10-12 22:25:10 +0300

    configuring lambda and apigateway

--- bin/sauna_app-iac.ts
+++ bin/sauna_app-iac.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { SaunaAppIacStack } from '../lib/sauna_app-iac-stack';
+import { SaunaAppStack } from '../lib/sauna_app-iac-stack';
 
 const app = new cdk.App();
-new SaunaAppIacStack(app, 'SaunaAppIacStack', {
+new SaunaAppStack(app, 'SaunaAppStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/sauna_app-iac-stack.ts
+++ lib/sauna_app-iac-stack.ts
@@ -1,16 +1,49 @@
-import * as cdk from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { CorsHttpMethod, HttpApi } from 'aws-cdk-lib/aws-apigatewayv2';
+import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import { HttpMethod } from 'aws-cdk-lib/aws-events';
 
-export class SaunaAppIacStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class SaunaAppStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+  // Create a Lambda function
+    const myLambda = new lambda.Function(this, 'MyFunction', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'app.lambda_handler',
+      code: lambda.Code.fromAsset('./my_function/lib'),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SaunaAppIacQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  // Create an API Gateway
+    const httpApi = new HttpApi(this, "MyApi", {
+      apiName: "My API",
+      corsPreflight: {
+        allowMethods: [
+          CorsHttpMethod.GET,
+          CorsHttpMethod.DELETE,
+          CorsHttpMethod.PUT,
+          CorsHttpMethod.POST,
+        ],
+        allowOrigins: ["*"],
+      },
+    });
+
+    const templateLambdaIntegration = new HttpLambdaIntegration('TemplateIntegration', myLambda);
+
+    // Create a resource and method for the API
+    httpApi.addRoutes({
+        path: '/invoke',
+        methods: [ HttpMethod.GET],
+        integration: templateLambdaIntegration,
+
+    })
+
+    // Output the API endpoint URL
+    new CfnOutput(this, "ApiEndpoint", {
+      value: httpApi.apiEndpoint,
+    });
+
   }
 }
