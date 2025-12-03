cc9b9df688fd26f5a9c239831f1799c37eda28da
Saija Pajari <saija.m.pajari@gmail.com>
2025-12-03 19:29:11 +0200

    adding scripts

---
+++ scripts/dynamodb-local-create.sh
@@ -0,0 +1,1 @@
+docker run -p 8000:8000 --network=lambda-local --volume ./dynamodb_data:/home/dynamodblocal/data --name dynamodb amazon/dynamodb-local  -jar DynamoDBLocal.jar -sharedDb -dbPath /home/dynamodblocal/data/
