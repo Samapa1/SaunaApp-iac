c69cdae6f9f988afc260deccc8592c3f2641e88c
Tommi Räsänen <tommi.v.rasanen@outlook.com>
2025-12-03 20:21:20 +0200

    update local dynamodb script

--- scripts/dynamodb-local-create.sh
+++ scripts/dynamodb-local-create.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env bash
+mkdir -p dynamodb_data
+docker network create lambda-local
 docker run -p 8000:8000 --network=lambda-local --volume ./dynamodb_data:/home/dynamodblocal/data --name dynamodb amazon/dynamodb-local  -jar DynamoDBLocal.jar -sharedDb -dbPath /home/dynamodblocal/data/
