92cbcd7dd5d268b000a85b295daf526cb0141840
Tommi Räsänen <tommi.v.rasanen@outlook.com>
2025-12-17 19:31:22 +0200

    fix create table script

--- scripts/dynamodb-local-tables.sh
+++ scripts/dynamodb-local-tables.sh
@@ -1,1 +1,2 @@
-aws dynamodb create-table --cli-input-json file://json/create-sauna-table.json --endpoint-url http://localhost:8000
+#!/usr/bin/env bash
+aws dynamodb create-table --cli-input-json file://../json/create-sauna-table.json --endpoint-url http://localhost:8000
