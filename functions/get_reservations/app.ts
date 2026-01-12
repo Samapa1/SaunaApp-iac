be859e362a221a8775acc97eef37a24a2f917569
Saija Pajari <saija.m.pajari@gmail.com>
2026-01-12 15:42:06 +0200

    fixing queries and refactoring

--- functions/get_reservations/app.ts
+++ functions/get_reservations/app.ts
@@ -8,48 +8,25 @@ export const client = DynamoDBDocumentClient.from(new DynamoDBClient({
 }));
 
 export const lambda_handler: APIGatewayProxyHandler = async (event, context) => {
-    console.log("Testing getreservations function")
     const sauna = event.queryStringParameters?.sauna
-    const dateData = event.queryStringParameters?.date 
-    console.log("dateData" + dateData)
-    console.log(dateData?.split('-')[0])
-    console.log(dateData?.split('-')[2])
-    const modifiedDateData = DateTime.fromObject({
-        weekYear: Number(dateData?.split('-')[0])+1,
-        weekNumber: Number(dateData?.split('-')[2])
-        });
-    const firstDayOfTheWeek= modifiedDateData.startOf('week');
-    const lastDayOfTheWeek= firstDayOfTheWeek.plus({days: 6})
 
-    const nextQueryInput = {
-        TableName: 'SaunaTable',
-        KeyConditionExpression: 'Id = :id and begins_with(#dynamoDate, :date)',
-        ExpressionAttributeValues: {
-            ':id': sauna,
-            ':date': `${dateData}-`
-        },  
-        ExpressionAttributeNames: { "#dynamoDate": "Date" }
-    }
-    const command = new QueryCommand(nextQueryInput)
-    const response = await client.send(command);
-
-    if (firstDayOfTheWeek.month !== lastDayOfTheWeek.month) {
-        console.log("different month")
-        const nextmonthDateData = `${lastDayOfTheWeek.year}-0${lastDayOfTheWeek.month}-${dateData?.split('-')[2]}`
+    const getReservations = async (data: string) => {    
         const nextQueryInput = {
             TableName: 'SaunaTable',
             KeyConditionExpression: 'Id = :id and begins_with(#dynamoDate, :date)',
             ExpressionAttributeValues: {
                 ':id': sauna,
-                ':date': `${nextmonthDateData}-`
+                ':date': `${data}-`
             },  
             ExpressionAttributeNames: { "#dynamoDate": "Date" }
         }
+        
         const command = new QueryCommand(nextQueryInput)
-        const nextResponse = await client.send(command);
+        const response = await client.send(command);
+        return response.Items
+    }
 
-        const retrievedItems = response.Items
-        const allItems = [...(retrievedItems || []), ...(nextResponse.Items || [])];
+    const formatResult = (allItems: any[]) => {   
         const reservationsForSauna = allItems?.filter(r => {
             return (Number(r.Id) === Number(sauna))
         })
@@ -71,29 +48,42 @@ export const lambda_handler: APIGatewayProxyHandler = async (event, context) => 
             statusCode: 200
         }
 
-    } else {
-        console.log("same month")
-        const retrievedItems = response.Items
-        const reservationsForSauna = retrievedItems?.filter(r => {
-        return (Number(r.Id) === Number(sauna))
-    })
+     } 
 
-    const resultFormatted = reservationsForSauna?.map(r => {
-        const dateParts = r.Date.split('-')
-        return ({
-            Id: r.Id,
-            Date: `${dateParts[3]}-${dateParts[1]}-${dateParts[0]}-${dateParts[4]}`
-        })
-    } ) 
- 
-    return {
-        headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Headers": "*"
-        },
-        body: JSON.stringify(resultFormatted),
-        statusCode: 200
-    }
+    const dateData = event.queryStringParameters?.date 
+    if (!dateData) 
+          return {
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Headers": "*"
+            },
+            body: JSON.stringify("No date provided"),
+            statusCode: 400
+        }
+
+    const modifiedDateData = DateTime.fromObject({
+        weekYear: Number(dateData?.split('-')[0]),
+        weekNumber: Number(dateData?.split('-')[2])
+        });
+    const firstDayOfTheWeek= modifiedDateData.startOf('week');
+    const lastDayOfTheWeek= firstDayOfTheWeek.plus({days: 6})
+
+
+    if (firstDayOfTheWeek.month !== lastDayOfTheWeek.month && dateData !== undefined) {
+        const firstMonthDateData = `${firstDayOfTheWeek.year}-${String(firstDayOfTheWeek.month).padStart(2, '0')}-${firstDayOfTheWeek.weekNumber}`
+        const firstResponseItems = await getReservations(firstMonthDateData);
+        const nextmonthDateData = `${lastDayOfTheWeek.year}-${String(lastDayOfTheWeek.month).padStart(2, '0')}-${lastDayOfTheWeek.weekNumber}`
+        const nextResponse = await getReservations(nextmonthDateData);
+    
+        const allItems = [...(firstResponseItems || []), ...(nextResponse || [])];
+        const formattedResult = formatResult(allItems);
+        return formattedResult;
+   
+    } else {
+        const retrievedItems = await getReservations(dateData);
+        const formattedResult = formatResult(retrievedItems || []);
+        return formattedResult;
+
     } 
 
     
