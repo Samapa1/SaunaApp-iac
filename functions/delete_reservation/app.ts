ece52083844a5a05eaae59f8a8f8ed2bf256f5d9
Saija Pajari <saija.m.pajari@gmail.com>
2026-03-25 19:03:16 +0200

    improving error handling in delete-reservation

--- functions/delete_reservation/app.ts
+++ functions/delete_reservation/app.ts
@@ -3,6 +3,7 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, DeleteCommand } from "@aws-sdk/lib-dynamodb";
 import authorize from "../../utils/functions/authorization";
 import { CognitoAccessTokenPayload } from "aws-jwt-verify/jwt-model";
+import { DateTime } from "luxon";
 
 export const client = DynamoDBDocumentClient.from(new DynamoDBClient({
     endpoint: 'http://dynamodb:8000'
@@ -17,6 +18,7 @@ const createResponse = (statusCode: number, message: string) => ({
 });
 
 export const delete_reservation: APIGatewayProxyHandler = async (event, context) => {
+    console.log("Delete reservation")
     const authorizationHeader = event.headers['Authorization'];
     let authorized: CognitoAccessTokenPayload
 
@@ -29,16 +31,22 @@ export const delete_reservation: APIGatewayProxyHandler = async (event, context)
 
     const sauna = event.queryStringParameters?.sauna;
     const date = event.queryStringParameters?.date;
+    console.log(sauna, date, "sauna and date from query parameters");
 
-    if (!sauna || !date) {
+     if (!sauna || !date) {
         return createResponse(400, "Please enter sauna and date");
     }
+
+    const dateParts = date.split('-');
+    const withoutHoursData = dateParts.slice(0, 3);
+    const newWeekNumber = DateTime.fromISO(withoutHoursData.join('-')).weekNumber;
+    const dateWithWeekNumber = `${withoutHoursData[0]}-${withoutHoursData[1]}-${newWeekNumber}-${withoutHoursData[2]}-${dateParts[3]}`;
 
     const command = new DeleteCommand({
         TableName: "SaunaTable",
         Key: {
             Id: sauna,
-            Date: date
+            Date: dateWithWeekNumber
         },
         ConditionExpression: "(attribute_not_exists(Id) AND attribute_not_exists(#Date)) OR UserId = :authorizedUserId",
         ExpressionAttributeValues: {
@@ -56,10 +64,15 @@ export const delete_reservation: APIGatewayProxyHandler = async (event, context)
         if (!response.Attributes) {
             return createResponse(404, `Reservation for sauna ${sauna} ${date} not found.`);
         }
-
         return createResponse(200, `Reservation for sauna ${sauna} ${date} removed.`);
     } catch (err) {
         console.error('Error deleting reservation:', err);
+        if (err instanceof Error) {
+            if (err.name === "ConditionalCheckFailedException") {
+                return createResponse(403, "You are not authorized to delete this reservation");
+            }
+        }
         return createResponse(500, "Something went wrong");
     }
+    
 }
