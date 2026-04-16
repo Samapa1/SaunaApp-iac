0f82974f28c68741b51f08e79406fca011ba15c4
Saija Pajari <saija.m.pajari@gmail.com>
2026-04-16 20:19:08 +0300

    fixing headers and dynamoDB endpoint

--- functions/make_reservations/app.ts
+++ functions/make_reservations/app.ts
@@ -5,9 +5,9 @@ import { DateTime } from 'luxon';
 import authorize from "../../utils/functions/authorization";
 import { CognitoAccessTokenPayload } from "aws-jwt-verify/jwt-model";
 
-export const client = DynamoDBDocumentClient.from(new DynamoDBClient({
-    // endpoint: 'http://dynamodb:8000'
-}));
+export const client = DynamoDBDocumentClient.from(new DynamoDBClient(process.env.ENDPOINT ?{
+    endpoint: process.env.ENDPOINT
+} : {}));
 
 const createResponse = (statusCode: number, message: string) => ({
     headers: {
@@ -19,7 +19,7 @@ const createResponse = (statusCode: number, message: string) => ({
 
 export const make_reservation: APIGatewayProxyHandler = async (event, context) => {
     console.log("make_reservation function");
-    const authorizationHeader = event.headers['authorization'];
+    const authorizationHeader = event.headers['authorization'] || event.headers['Authorization'];
 
     let authorized: CognitoAccessTokenPayload
 
